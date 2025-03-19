86aad8f128c35337296e9940da85c6125b3eb115
Sharon812 <sharusharon1110@gmail.com>
2025-03-19 22:09:18 +0530

    added ejs setup and nocache setup in server.js

--- server.js
+++ server.js
@@ -12,10 +12,32 @@ dotenv.config();
 import { connectDB } from "./src/infrastructure/database/connect_db.js";
 connectDB();
 
+// Fix __dirname in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//for no cache
+app.use((req, res, next) => {
+  res.set("cache-control", "no-store");
+  next();
+});
+
+//for public folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//for ejs setup
+app.set("view engine", "ejs");
+app.set("views", [path.join(__dirname, "src", "views/clinic")]);
+//pass other things like array itself
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//for clinic routes
+// import clinicRoute from "./src/interfaces/routes/clinic_routes.js";
+// app.use("/clinic", clinicRoute);
+
 //to listen to server
 app.listen(process.env.PORT, "0.0.0.0", () => {
-  console.log("server running");
+  console.log(`server running at port ${process.env.PORT}`);
 });
