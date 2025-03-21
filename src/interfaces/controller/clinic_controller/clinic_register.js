0c641b76e28e95bde048f4be2dd1bec03cf7c0b1
Sharon812 <sharusharon1110@gmail.com>
2025-03-21 21:41:25 +0530

    added registration page 2nd and third step files

--- src/interfaces/controller/clinic_controller/clinic_register.js
+++ src/interfaces/controller/clinic_controller/clinic_register.js
@@ -1,3 +1,11 @@
 export const renderRegisterPage = async (req, res) => {
   res.render("clinic_loginPage");
 };
+
+export const renderRegisterPageSecondStep = async (req, res) => {
+  res.render("clinic_registrationPageSecond");
+};
+
+export const renderRegisterPageThirdStep = async (req, res) => {
+  res.render("clinic_registrationPageThird");
+};
--- src/interfaces/routes/clinic_routes.js
+++ src/interfaces/routes/clinic_routes.js
@@ -1,7 +1,13 @@
 import express from "express";
 const clinicRoute = express.Router();
-import { renderRegisterPage } from "../controller/clinic_controller/clinic_register.js";
+import {
+  renderRegisterPage,
+  renderRegisterPageSecondStep,
+  renderRegisterPageThirdStep,
+} from "../controller/clinic_controller/clinic_register.js";
 
 clinicRoute.get("/signup", renderRegisterPage);
+clinicRoute.get("/signup2", renderRegisterPageSecondStep);
+clinicRoute.get("/signup3", renderRegisterPageThirdStep);
 
 export default clinicRoute;
