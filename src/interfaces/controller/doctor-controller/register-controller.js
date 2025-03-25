b6d46ec22c59c3aed4fc18ac51982adadc6f5e48
Aljo00 <aljojoju950@gmail.com>
2025-03-25 21:58:20 +0530

    feat: enhance doctor registration controller with POST method and refactor naming

--- src/interfaces/controller/doctor-controller/register-controller.js
+++ src/interfaces/controller/doctor-controller/register-controller.js
@@ -1,6 +1,9 @@
-class DoctorResisterController {
-  constructor() {}
+class DoctorRegisterController {
+  constructor(createDoctorUseService) {
+    this.createDoctorUseService = createDoctorUseService;
+  }
 
+  // Doctor Register Page Render (GET Request)
   registerPageRender = async (req, res) => {
     try {
       console.log("Doctor register page route hit");
@@ -10,6 +13,28 @@ class DoctorResisterController {
       res.status(500).send("Internal Server Error");
     }
   };
+
+  // Doctor Register (POST Request)
+  registerDoctor = async (req, res) => {
+    try {
+      console.log(req.body);
+      const { name, email, phone, password } = req.body;
+      const doctor = await this.createDoctorUseService.execute({
+        name,
+        email,
+        phone,
+        password,
+      });
+
+      res.status(201).json({
+        message: "Doctor Registered Successfully",
+        doctorId: doctor._id,
+      });
+    } catch (error) {
+      console.log("registerDoctor Controller Error:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  };
 }
 
-export const doctorRegisterController = new DoctorResisterController();
+export const doctorRegisterController = new DoctorRegisterController();
--- src/interfaces/routes/doctor-routes.js
+++ src/interfaces/routes/doctor-routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { doctorLoginController } from "../controller/doctor_controller/login-controller.js";
-import { doctorRegisterController } from "../controller/doctor_controller/register-controller.js";
+import { doctorLoginController } from "../controller/doctor-controller/login-controller.js";
+import { doctorRegisterController } from "../controller/doctor-controller/register-controller.js";
 
 const router = express.Router();
 
 router.get("/login", doctorLoginController.loginPageRender);
 
 router.get("/register", doctorRegisterController.registerPageRender);
+
+router.post("/register", doctorRegisterController.registerDoctor);
 
 export const doctorRoute = router;
